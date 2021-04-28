34e66e61e6e18fd6ee497708c0e2695772d30cfe
TranBinhLuatUIT <76168991+TranBinhLuatUIT@users.noreply.github.com>
2021-04-28 10:48:48 +0700

    add func
    func check csv to verify before convert to df

--- main.py
+++ main.py
@@ -18,8 +18,29 @@ def convert_csv_to_df(path, source_type):
     df['source'] = source_type
     
     return df
+
 #test function, convert mobile csv to dataframe
+print(convert_csv_to_df("datasets/2020Q4_nps_mobile.csv", "mobile"))
+#It workes, so in the future, if I have any dataset from another source, I can easily create dataframe for it
 
-df = convert_csv_to_df("datasets/2020Q4_nps_mobile.csv", "mobile")
+#I have to check whether my csv file is valid (have 3 column response_date, user_id, nps_rating)
+def check_csv(path):
+    """Check whether your csv file include 3 column response_date, user_id, nps_rating or not
+
+    Args:
+        path(string) : path to your csv file
+        
+    Return:
+        bool: True if csv have 3 column, False if not
+        
+    """
+    #use context manager with
+    with open(path) as f:
+        first_line = f.readline()
+        if (first_line == "response_date,user_id,nps_rating\n"):
+            return True
+        else: return False
+
+print(check_csv("datasets/corrupted.csv"))
+        
     
-
